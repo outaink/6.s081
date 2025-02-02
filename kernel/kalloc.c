b54882c9e8e0ad57a4e59ec093e29398e9ed03f0
MO HUAIHAO <mohuaihaoxm@163.com>
2025-02-01 21:03:55 -0500

    lab 2

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,17 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+// 获取空闲内存
+// dst == destination address
+void mh_freebytes(uint64* dst) {
+  *dst = 0;
+  struct run* p = kmem.freelist;
+
+  acquire(&kmem.lock);  // 加锁保证线程安全
+  while (p) {
+    *dst += PGSIZE; 
+    p = p->next;
+  }
+  release(&kmem.lock);
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -696,3 +696,16 @@ procdump(void)
     printf("\n");
   }
 }
+
+
+// 统计处于活动状态的进程
+void 
+mh_procnum(uint64* dst) {
+  *dst = 0;
+  struct proc* p;
+  for (p = proc; p < &proc[NPROC]; p++) {
+    if (p->state != UNUSED) {
+      (*dst)++;
+    }
+  }
+}
